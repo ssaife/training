d547a7e764d6d28916791e8d45841dcdc6288f23
ssaife <ssaife@asaltech.com>
2020-06-22 10:12:21 +0300

    update

--- assignments/alienNumber/alianNumber.py
+++ assignments/alienNumber/alianNumber.py
@@ -1,3 +1,6 @@
+import os
+
+
 class AlianNumberTranslator(object):
     @staticmethod
     def translateAlianNumber(str_number, src_system, target_system):
@@ -8,21 +11,76 @@ class AlianNumberTranslator(object):
         :param target_system: string
         :return: string
         """
-
-        base_src_system = len(src_system)
-        base_target_system = len(target_system)
-        list_str_number = list(str_number)
+        # validate inputs
+        if not isinstance(str_number, str) \
+                or not isinstance(src_system, str) \
+                or not isinstance(target_system, str):
+            raise Exception("Invalid Input(s)")
+        # check duplicate in src and target systems
+        if len(list(src_system.strip())) is not len(set(src_system.strip())):
+            raise Exception("Duplicate i not accepted in src system")
+        if len(list(target_system.strip())) is not len(set(target_system.strip())):
+            raise Exception("Duplicate i not accepted in target system")
+        base_src_system = len(src_system.strip())
+        base_target_system = len(target_system.strip())
+        list_str_number = list(str_number.strip())
+        list_str_number.reverse()
         decimal_value_number = 0
+        index_list_str_number = 0
         for digit in list_str_number:
-            decimal_value_number += list_str_number.index(digit) * (base_src_system ** list_str_number.index(digit))
-            return str(decimal_value_number)
+            decimal_value_number += src_system.index(digit) * (base_src_system ** index_list_str_number)
+            index_list_str_number = index_list_str_number + 1
         list_indexes_result = []
-        location = 0
         while decimal_value_number > 0:
-            list_indexes_result[location] = decimal_value_number // base_target_system
-            decimal_value_number /= base_target_system
-            digit += 1
-        result = []
+            list_indexes_result.append(decimal_value_number % base_target_system)
+            decimal_value_number //= base_target_system
+        str_result = []  # type: List[str]
         for index in list_indexes_result:
-            result.append(target_system[index])
-        return "".join(result)
+            str_result.append(target_system[index])
+        str_result.reverse()
+        return "".join(str_result).lstrip(target_system[0])
+
+
+class FileTester:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def testFile(filename):
+        """
+
+        :param filename: string
+        :return: None
+        """
+        # validate input
+        if not isinstance(filename, str):
+            raise Exception("Invalid Input: File name "
+                            "should be in string format")
+        # check existence
+        if not os.path.isfile(filename):
+            raise Exception("File Not Found")
+        filein = open(filename, 'r')
+        # open output file, same name of the input file but with additional prefix
+        fileout = open(os.path.splitext(filename)[0] + 'Output.txt', 'w')
+        try:
+            number_test_cases = int(filein.readline().strip())
+        except Exception:
+            raise Exception("Number of test cases should be an integer")
+        # represents the index of the current case
+        index = 1
+        # loop over the file lines - each contains a string value represents the number to be tested
+        for input_case in filein:
+            split_input_case = input_case.strip().split()
+            # write the result into the output file without leading zeros
+            str_result = AlianNumberTranslator.translateAlianNumber(split_input_case[0]
+                                                                    , split_input_case[1]
+                                                                    , split_input_case[2])
+            assert isinstance(str_result, str)
+            fileout.write("Case #" + str(index) + ": " + str_result + "\n")
+            index = index + 1
+            number_test_cases = number_test_cases - 1
+            if number_test_cases is 0:
+                break
+        # close opened files
+        filein.close()
+        fileout.close()
--- assignments/alienNumber/alianNumberTest.py
+++ assignments/alienNumber/alianNumberTest.py
@@ -1,10 +1,31 @@
 import unittest
-from alianNumber import AlianNumberTranslator
+from alianNumber import AlianNumberTranslator, FileTester
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(AlianNumberTranslator.translateAlianNumber('f', '0f8', '0123456789'), '1')
+    def testAlianNumberTranslator(self):
+        self.assertEqual(AlianNumberTranslator.translateAlianNumber('1011', '01', '0123456789'), '11')
+        self.assertEqual(AlianNumberTranslator.translateAlianNumber('foo', 'of8', '0123456789'), '9')
+        self.assertEqual(AlianNumberTranslator.translateAlianNumber('foo', 'of8', 'of8'), 'foo')
+        self.assertEqual(AlianNumberTranslator.translateAlianNumber('ff', '0123456789abcdef', '01'), '11111111')
+
+    def testInvalidInputException(self):
+        with self.assertRaises(Exception):
+            AlianNumberTranslator.translateAlianNumber(22, '235', '123')
+        with self.assertRaises(Exception):
+            AlianNumberTranslator.translateAlianNumber('22', 12, '123')
+        with self.assertRaises(Exception):
+            AlianNumberTranslator.translateAlianNumber('22', '235', 123)
+
+    def testDuplicateException(self):
+        with self.assertRaises(Exception):
+            AlianNumberTranslator.translateAlianNumber('22', '1223', '123')
+        with self.assertRaises(Exception):
+            AlianNumberTranslator.translateAlianNumber('2', '123', '1233')
+
+    def testInputFile(self):
+        FileTester.testFile('A-large-practice.in')
+        FileTester.testFile('A-small-practice.in')
 
 
 if __name__ == '__main__':
