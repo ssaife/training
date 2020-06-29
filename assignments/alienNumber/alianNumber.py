f816f1e15cb00599ca7de59b1a2a3e8bd05faaf9
ssaife <ssaife@asaltech.com>
2020-06-29 10:33:56 +0300

    update

--- assignments/alienNumber/alianNumber.py
+++ assignments/alienNumber/alianNumber.py
@@ -2,85 +2,117 @@ import os
 
 
 class AlianNumberTranslator(object):
-    @staticmethod
-    def translateAlianNumber(str_number, src_system, target_system):
-        """
+    def __init__(self, str_number, str_src_system, str_target_system):
+        self._str_number = str_number.strip()
+        self._str_src_system = str_src_system.strip()
+        self._str_target_system = str_target_system.strip()
+        self._list_str_number = self._extract_reversed_list()
 
-        :param str_number: string
-        :param src_system: string
-        :param target_system: string
-        :return: string
-        """
-        # validate inputs
-        if not isinstance(str_number, str) \
-                or not isinstance(src_system, str) \
-                or not isinstance(target_system, str):
-            raise Exception("Invalid Input(s)")
-        # check duplicate in src and target systems
-        if len(list(src_system.strip())) is not len(set(src_system.strip())):
-            raise Exception("Duplicate i not accepted in src system")
-        if len(list(target_system.strip())) is not len(set(target_system.strip())):
-            raise Exception("Duplicate i not accepted in target system")
-        base_src_system = len(src_system.strip())
-        base_target_system = len(target_system.strip())
-        list_str_number = list(str_number.strip())
-        list_str_number.reverse()
-        decimal_value_number = 0
-        index_list_str_number = 0
-        for digit in list_str_number:
-            decimal_value_number += src_system.index(digit) * (base_src_system ** index_list_str_number)
-            index_list_str_number = index_list_str_number + 1
+    def translate(self):
+
+        self._validate_inputs()
+        self._check_duplicate_in_src_and_target_systems()
+        base_src_system, base_target_system = self._calculate_src_and_target_bases()
+        decimal_value_number = self._convert_to_decimal(base_src_system)
+        list_indexes_result = self._convert_from_decimal_to_list_of_indexes_of_target_system(base_target_system,
+                                                                                             decimal_value_number)
+        str_result_without_leading_zeros = self._convert_from_list_of_indexes_to_string_result(list_indexes_result)
+        return str_result_without_leading_zeros
+
+    def _convert_from_list_of_indexes_to_string_result(self, list_indexes_result):
+        str_result = []
+        for index in list_indexes_result:
+            str_result.append(self._str_target_system[index])
+        str_result.reverse()
+        str_result_without_leading_zeros = "".join(str_result).lstrip(self._str_target_system[0])
+        return str_result_without_leading_zeros
+
+    def _convert_from_decimal_to_list_of_indexes_of_target_system(self, base_target_system, decimal_value_number):
         list_indexes_result = []
         while decimal_value_number > 0:
             list_indexes_result.append(decimal_value_number % base_target_system)
             decimal_value_number //= base_target_system
-        str_result = []  # type: List[str]
-        for index in list_indexes_result:
-            str_result.append(target_system[index])
-        str_result.reverse()
-        return "".join(str_result).lstrip(target_system[0])
+        return list_indexes_result
+
+    def _convert_to_decimal(self, base_src_system):
+        decimal_value_number = 0
+        running_index = 0
+        for digit in self._list_str_number:
+            decimal_value_number += self._str_src_system.index(digit) * (base_src_system ** running_index)
+            running_index = running_index + 1
+        return decimal_value_number
+
+    def _extract_reversed_list(self):
+        list_str_number = list(self._str_number)
+        list_str_number.reverse()
+        return list_str_number
+
+    def _calculate_src_and_target_bases(self):
+        base_src_system = len(self._str_src_system)
+        base_target_system = len(self._str_target_system)
+        return base_src_system, base_target_system
+
+    def _check_duplicate_in_src_and_target_systems(self):
+        if len(list(self._str_src_system.strip())) is not len(set(self._str_src_system.strip())):
+            raise Exception("Duplicate is not accepted in src system")
+        if len(list(self._str_target_system.strip())) is not len(set(self._str_target_system.strip())):
+            raise Exception("Duplicate is not accepted in target system")
+
+    def _validate_inputs(self):
+        # validate inputs
+        if not isinstance(self._str_number, str) \
+                or not isinstance(self._str_src_system, str) \
+                or not isinstance(self._str_target_system, str):
+            raise Exception("Invalid Input(s)")
 
 
 class FileTester:
-    def __init__(self):
+    def __init__(self, file_name):
+        self._file_name = file_name
         pass
 
-    @staticmethod
-    def testFile(filename):
-        """
+    def test_File(self):
+        self._validate_input()
+        self._check_file_existence()
+        filein, fileout = self._open_input_and_output_files()
+        number_test_cases = self._read_number_of_test_cases(filein)
+        self._convert_cases_in_input_file(filein, fileout, number_test_cases)
+        # close opened files
+        self._close_input_and_output_files(filein, fileout)
 
-        :param filename: string
-        :return: None
-        """
-        # validate input
-        if not isinstance(filename, str):
+    def _open_input_and_output_files(self):
+        filein = open(self._file_name, 'r')
+        fileout = open(os.path.splitext(self._file_name)[0] + 'Output.txt', 'w')
+        return filein, fileout
+
+    def _check_file_existence(self):
+        if not os.path.isfile(self._file_name):
+            raise Exception("File Not Found")
+
+    def _validate_input(self):
+        if not isinstance(self._file_name, str):
             raise Exception("Invalid Input: File name "
                             "should be in string format")
-        # check existence
-        if not os.path.isfile(filename):
-            raise Exception("File Not Found")
-        filein = open(filename, 'r')
-        # open output file, same name of the input file but with additional prefix
-        fileout = open(os.path.splitext(filename)[0] + 'Output.txt', 'w')
+
+    def _close_input_and_output_files(self, filein, fileout):
+        filein.close()
+        fileout.close()
+
+    def _convert_cases_in_input_file(self, filein, fileout, number_test_cases):
+        running_index = 1
+        for input_case in filein:
+            split_input_case = input_case.strip().split()
+            str_result = AlianNumberTranslator(split_input_case[0], split_input_case[1], split_input_case[2]).translate()
+            assert isinstance(str_result, str)
+            fileout.write("Case #" + str(running_index) + ": " + str_result + "\n")
+            running_index = running_index + 1
+            number_test_cases = number_test_cases - 1
+            if number_test_cases is 0:
+                break
+
+    def _read_number_of_test_cases(self, filein):
         try:
             number_test_cases = int(filein.readline().strip())
         except Exception:
             raise Exception("Number of test cases should be an integer")
-        # represents the index of the current case
-        index = 1
-        # loop over the file lines - each contains a string value represents the number to be tested
-        for input_case in filein:
-            split_input_case = input_case.strip().split()
-            # write the result into the output file without leading zeros
-            str_result = AlianNumberTranslator.translateAlianNumber(split_input_case[0]
-                                                                    , split_input_case[1]
-                                                                    , split_input_case[2])
-            assert isinstance(str_result, str)
-            fileout.write("Case #" + str(index) + ": " + str_result + "\n")
-            index = index + 1
-            number_test_cases = number_test_cases - 1
-            if number_test_cases is 0:
-                break
-        # close opened files
-        filein.close()
-        fileout.close()
+        return number_test_cases
--- assignments/alienNumber/alianNumberTest.py
+++ assignments/alienNumber/alianNumberTest.py
@@ -4,28 +4,30 @@ from alianNumber import AlianNumberTranslator, FileTester
 
 class MyTestCase(unittest.TestCase):
     def testAlianNumberTranslator(self):
-        self.assertEqual(AlianNumberTranslator.translateAlianNumber('1011', '01', '0123456789'), '11')
-        self.assertEqual(AlianNumberTranslator.translateAlianNumber('foo', 'of8', '0123456789'), '9')
-        self.assertEqual(AlianNumberTranslator.translateAlianNumber('foo', 'of8', 'of8'), 'foo')
-        self.assertEqual(AlianNumberTranslator.translateAlianNumber('ff', '0123456789abcdef', '01'), '11111111')
+        self.assertEqual(AlianNumberTranslator('1011', '01', '0123456789').translate(), '11')
+        self.assertEqual(AlianNumberTranslator('foo', 'of8', '0123456789').translate(), '9')
+        self.assertEqual(AlianNumberTranslator('foo', 'of8', 'of8').translate(), 'foo')
+        self.assertEqual(AlianNumberTranslator('ff', '0123456789abcdef', '01').translate(), '11111111')
 
     def testInvalidInputException(self):
         with self.assertRaises(Exception):
-            AlianNumberTranslator.translateAlianNumber(22, '235', '123')
+            AlianNumberTranslator(22, '235', '123').translate()
         with self.assertRaises(Exception):
-            AlianNumberTranslator.translateAlianNumber('22', 12, '123')
+            AlianNumberTranslator('22', 12, '123').translate()
         with self.assertRaises(Exception):
-            AlianNumberTranslator.translateAlianNumber('22', '235', 123)
+            AlianNumberTranslator('22', '235', 123).translate()
 
     def testDuplicateException(self):
         with self.assertRaises(Exception):
-            AlianNumberTranslator.translateAlianNumber('22', '1223', '123')
+            AlianNumberTranslator('22', '1223', '123').translate()
         with self.assertRaises(Exception):
-            AlianNumberTranslator.translateAlianNumber('2', '123', '1233')
+            AlianNumberTranslator('2', '123', '1233').translate()
 
     def testInputFile(self):
-        FileTester.testFile('A-large-practice.in')
-        FileTester.testFile('A-small-practice.in')
+        FileTester('A-large-practice.in').test_File()
+        FileTester('A-small-practice.in').test_File()
+        with self.assertRaises(Exception):
+            FileTester('123').test_File()
 
 
 if __name__ == '__main__':
